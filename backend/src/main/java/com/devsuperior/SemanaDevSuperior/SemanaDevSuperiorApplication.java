8d93595331bff846cc5be3abe06a01baadf77297
GiuliaMarcela <giuliamendes67@gmail.com>
2021-01-04 18:01:14 -0300

    Project created.

---
+++ backend/src/main/java/com/devsuperior/SemanaDevSuperior/SemanaDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.SemanaDevSuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemanaDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemanaDevSuperiorApplication.class, args);
+	}
+
+}
