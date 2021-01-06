a7749f4fa28bd05e6be0ada76b8beeb5e2e7dd94
GiuliaMarcela <giuliamendes67@gmail.com>
2021-01-05 23:52:17 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/SemanaDevSuperior/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/SemanaDevSuperior/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.devsuperior.SemanaDevSuperior.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.SemanaDevSuperior.dto.OrderDTO;
 import com.devsuperior.SemanaDevSuperior.services.OrderService;
@@ -22,5 +26,13 @@ public class OrderController {
 	public ResponseEntity<List<OrderDTO>> findAllOrders() {
 		List<OrderDTO> list = order.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO entityDTO){
+		entityDTO = order.insert(entityDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(entityDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(entityDTO);
 	}
 }
--- backend/src/main/java/com/devsuperior/SemanaDevSuperior/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/SemanaDevSuperior/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.SemanaDevSuperior.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,18 +9,37 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.SemanaDevSuperior.dto.OrderDTO;
+import com.devsuperior.SemanaDevSuperior.dto.ProductDTO;
 import com.devsuperior.SemanaDevSuperior.entities.Order;
+import com.devsuperior.SemanaDevSuperior.entities.Product;
+import com.devsuperior.SemanaDevSuperior.enums.OrderStatus;
 import com.devsuperior.SemanaDevSuperior.repositories.OrderRepository;
+import com.devsuperior.SemanaDevSuperior.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 
 	@Autowired
-	private OrderRepository order; 
+	private OrderRepository repOrder; 
+	
+	@Autowired
+	private ProductRepository repProduct;
 	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
-		List<Order> list = order.findOrdersWithProducts();
+		List<Order> list = repOrder.findOrdersWithProducts();
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public OrderDTO insert(OrderDTO entityDTO) {
+		Order order = new Order(null, entityDTO.getAddress(), entityDTO.getLatitude(), entityDTO.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for(ProductDTO products : entityDTO.getProducts()) {
+			Product product = repProduct.getOne(products.getId());
+			order.getProducts().add(product);
+		}
+		order = repOrder.save(order);
+		return new OrderDTO(order);
 	}
 } 
