4ea726a4c47c31de398416f7872f08acae728663
GiuliaMarcela <giuliamendes67@gmail.com>
2021-01-07 13:26:16 -0300

    Add Navbar

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
-  return <div className="App">Hello human!</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend-web/src/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import { ReactComponent as Logo } from "../assets/images/Group.svg";
+import "./styles.css";
+
+const Navbar = () => {
+  return (
+    <nav className="main-navbar">
+      <Logo />
+      <a href="home" className="logo-text">
+        DS Delivery
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
