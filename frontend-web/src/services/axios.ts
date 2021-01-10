190e78c81a343135b97d4875679d72a7eab4f3e1
GiuliaMarcela <giuliamendes67@gmail.com>
2021-01-09 21:20:37 -0300

    Add env vars

--- frontend-web/src/services/axios.ts
+++ frontend-web/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { OrderPayload } from "../pages/Orders/types";
 
-const base_URL = "https://localhost:8080";
+const base_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export const fetchProducts = () => {
